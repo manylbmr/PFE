b25649fa39c674001f4685a6bc750492ec993f01
lemmouchimds <lemmouchimds@gmail.com>
2024-05-02 14:32:53 +0100

    Changing the login page

--- src/views/crafted/authentication/basic-flow/SignIn.vue
+++ src/views/crafted/authentication/basic-flow/SignIn.vue
@@ -16,13 +16,13 @@
         <!--end::Title-->
 
         <!--begin::Link-->
-        <div class="text-gray-500 fw-semibold fs-4">
-          New Here?
-
+        <!-- <div class="text-gray-500 fw-semibold fs-4">
+          New Here? -->
+        <!-- 
           <router-link to="/sign-up" class="link-primary fw-bold">
-            Create an Account
-          </router-link>
-        </div>
+            Create an Account -->
+        <!-- </router-link> -->
+        <!-- </div> -->
         <!--end::Link-->
       </div>
       <!--begin::Heading-->
@@ -62,13 +62,15 @@
         <!--begin::Wrapper-->
         <div class="d-flex flex-stack mb-2">
           <!--begin::Label-->
-          <label class="form-label fw-bold text-gray-900 fs-6 mb-0">Password</label>
+          <label class="form-label fw-bold text-gray-900 fs-6 mb-0"
+            >Password</label
+          >
           <!--end::Label-->
 
           <!--begin::Link-->
-          <router-link to="/password-reset" class="link-primary fs-6 fw-bold">
+          <!-- <router-link to="/password-reset" class="link-primary fs-6 fw-bold">
             Forgot Password ?
-          </router-link>
+          </router-link> -->
           <!--end::Link-->
         </div>
         <!--end::Wrapper-->
@@ -112,11 +114,11 @@
         <!--end::Submit button-->
 
         <!--begin::Separator-->
-        <div class="text-center text-muted text-uppercase fw-bold mb-5">or</div>
+        <!-- <div class="text-center text-muted text-uppercase fw-bold mb-5">or</div> -->
         <!--end::Separator-->
 
         <!--begin::Google link-->
-        <a
+        <!-- <a
           href="#"
           class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5"
         >
@@ -126,11 +128,11 @@
             class="h-20px me-3"
           />
           Continue with Google
-        </a>
+        </a> -->
         <!--end::Google link-->
 
         <!--begin::Google link-->
-        <a
+        <!-- <a
           href="#"
           class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5"
         >
@@ -140,18 +142,18 @@
             class="h-20px me-3"
           />
           Continue with Facebook
-        </a>
+        </a> -->
         <!--end::Google link-->
 
         <!--begin::Google link-->
-        <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100">
+        <!-- <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100">
           <img
             alt="Logo"
             :src="getAssetPath('media/svg/brand-logos/apple-black.svg')"
             class="h-20px me-3"
           />
           Continue with Apple
-        </a>
+        </a> -->
         <!--end::Google link-->
       </div>
       <!--end::Actions-->
@@ -238,7 +240,7 @@ export default defineComponent({
       //Deactivate indicator
       submitButton.value?.removeAttribute("data-kt-indicator");
       // eslint-disable-next-line
-        submitButton.value!.disabled = false;
+      submitButton.value!.disabled = false;
     };
 
     return {
